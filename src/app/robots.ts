736983ea8f51fb161b139e81048078ad37c2f07f
RadjiMohamedSalim013 <radjimohamed013@gmail.com>
2026-06-20 08:30:32 +0000

    ajout

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,15 +2,6 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-      'max-video-preview': -1,
-    },
     rules: [
       {
         userAgent: "*",
